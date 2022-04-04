4ede9f3a910a2c4ccd4d42a1bf84d703c54d5aa1
Erica L L <89658905+erodrig6@users.noreply.github.com>
2022-04-04 22:02:39 +0000

    midterm

--- README.md
+++ README.md
@@ -1,25 +1,30 @@
 #### CS 110
-# Chapter 4 - Lab - Functions
-
-### [Assignment Description](https://docs.google.com/document/d/1V20D_upUX4MO8YmskKlRB25Yu2pCEv3-h8z4EAfrSno/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Creating a grid function and cos sin and tangent curve  >
+What did you clean up?: 
+All changes were made using comments so that the changes are visible.
+- Eliminated redundencies in functions called. The drawSineCurve function was called more than once before proper set up of the window was completed and that has now been fixed.
+- Eliminated redundencies in the setup of the window. The setupWindow function was being given parameters that were either not used or not needed.
+- Code that had set the speed or shape of the turtle object were commented out so that the two added functions would work as intended.
 
-## GRACE DAYS
-Grace days used for this assignment: < 0 >
+Summary of function(s) added: 
+The added function asks the user what shape they would like their turtle to be using corresponding numbers. After the shape is selected, the code prints what the new shape is and uses it to draw sin, cos, and tan curves. 
 
-Grace days remaining: < 5 >/5
+Summary of Feature Added: 
+Added a function in which changes the speed the turtle draws sin, cos, and tan line. It can be any speed bewtween 1 and 10, which is chosen by the user of the code.  
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < N/A >
+ N/A
 
 ## REFERENCES:
- < geeksforgeeks >
+ turtle library, geeksforgeeks 
 
 ## MISCELLANEOUS COMMENTS:
- < Na >
+N/A
+
+
--- main.py
+++ main.py
@@ -5,36 +5,57 @@ import math
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+def changeShape(turt):
+  shapeRequested = input("To select a different turtle shape, please input the corresponding number\n1.) Square\n2.) Arrow\n3.) Circle\n4.) Turtle\n")
+  if shapeRequested == "1":
+    turt.shape('square')
+    print("The new shape is:\nSquare!")
+  elif shapeRequested == "2":
+    turt.shape('arrow')
+    print("The new shape is:\nArrow!")
+  elif shapeRequested == "3":
+    turt.shape('circle')
+    print("The new shape is:\nCircle!")
+  elif shapeRequested == "4":
+    turt.shape('turtle')
+    print("The new shape is:\nTurtle!")
+  else:
+    print("I'm sorry, that input is not recognized.")
 
-def drawSineCurve(meg):
-  win = turtle.Screen()
-  meg.penup()
-  meg.goto(-360, math.sin(math.radians(-360)))
-  meg.pendown()
-  for i in range(-360, 361):
-    meg.goto(i, math.sin(math.radians(i)))
+def changeSpeed(turt):
+  speedRequested = input("Select a speed, 1 through 10. The higher the number, the faster the speed. Only integer values are acceptable.\n")
+  speedInt = int(speedRequested)
+  if speedInt > 0 and speedInt < 11:
+    turt.speed(speedInt)
+  else:
+    print("Invalid.")
 
-def drawCosineCurve(meg):
-  win = turtle.Screen()
-  meg.penup()
-  meg.goto(-360, math.cos(math.radians(-360)))
-  meg.pendown()
+def drawSineCurve(turt):
+  turt.penup()
+  turt.goto(-360, math.sin(math.radians(-360)))
+  turt.pendown()
   for i in range(-360, 361):
-    meg.goto(i, math.cos(math.radians(i)))
+    turt.goto(i, math.sin(math.radians(i)))
 
-def drawTangentCurve(meg):
-  win = turtle.Screen()
-  meg.penup()
-  meg.goto(-360, math.tan(math.radians(-360)))
-  meg.pendown()
+def drawCosineCurve(turt):
+  turt.penup()
+  turt.goto(-360, math.cos(math.radians(-360)))
+  turt.pendown()
   for i in range(-360, 361):
-    meg.goto(i, math.tan(math.radians(i)))
+    turt.goto(i, math.cos(math.radians(i)))
+
+def drawTangentCurve(turt):
+  turt.penup()
+  turt.goto(-360, math.tan(math.radians(-360)))
+  turt.pendown()
+  for i in range(-360, 361):
+    turt.goto(i, math.tan(math.radians(i)))
   
-def setupWindow(win):
-    win = turtle.setworldcoordinates(-360, -1, 360, 1)
+def setupWindow():
+    wn = turtle.setworldcoordinates(-360, -1, 360, 1)
 
 def setupAxis(turtle_axis):
-  turtle_axis.shape('turtle')
+ ### turtle_axis.shape('turtle')
   for i in range(2):
     turtle_axis.goto(0, 0)
     turtle_axis.right(90)
@@ -49,15 +70,18 @@ def main():
     wn = turtle.Screen()
     wn.tracer(5)
     dart = turtle.Turtle()
-    dart.speed(0)
-    drawSineCurve(dart)
+    changeSpeed(dart)
+    changeShape(dart)
+    #dart.speed(0)
+    #drawSineCurve(dart)
 
     #Part B
-    setupWindow(wn)
+    setupWindow()
     setupAxis(dart)
-    dart.speed(0)
+    #dart.speed(0)
     drawSineCurve(dart)
     drawCosineCurve(dart)
     drawTangentCurve(dart)
     wn.exitonclick()
 main()
+    
